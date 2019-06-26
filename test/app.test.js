e7aa9b46cc35ca87082d66427bffa7991fbf789f
DannySuarez <suarezd10@gmail.com>
2019-06-26 15:57:21 -0700

    exercise3

--- test/app.test.js
+++ test/app.test.js
@@ -2,11 +2,22 @@ const request = require('supertest');
 const app = require ('../lib/app');
 
 describe('something', () => {
-  it('returns a list of dogs', () => {
+  it('returns a list of dogs', done => {
     return request(app)
       .get('/')
       .then(res => {
         expect(res.text).toEqual(expect.stringContaining('spot'));
+        done();
       });
   });
+
+  it('returns red', done => {
+    return request(app)
+      .get('/red')
+      .then(res => {
+        expect(res.text).toEqual(expect.stringContaining('red'));
+        done();
+      });
+  });
+
 });
