f2b9d2246b1876ac17bb16e50ebaeff45c1edd22
DannySuarez <suarezd10@gmail.com>
2019-06-27 10:26:05 -0700

    refactored code based on morning walkthrough

--- lab/app.js
+++ lab/app.js
@@ -1,25 +1,21 @@
 const { createServer } = require('http');
+const { getFilePath } = require('../lab/util/filePath');
 const { parse } = require('url');
 const fs = require('fs');
-
 
 const app = createServer((req, res) => {
   const { pathname } = parse(req.url);
   res.setHeader('Content-Type', 'text/html');
-  console.log(pathname);
 
-  if(pathname === '/index.html') {
+  res.statusCode = 200;
+  fs.readFile(getFilePath(pathname), { encoding: 'utf8' }, (err, data) => {
+    if(err && err.code === 'ENOENT') {
+      res.statusCode = 404;
+      res.end('No File');
+    }
+    res.end(data);
+  });
   
-    res.statusCode = 200;
-    fs.readFile('public/index.html', (err, data) => {
-      if(err) throw err;
-      res.end(data);
-      console.log('TCL: app -> data', data);
-    });
-  } else {
-    res.statusCode = 404;
-    res.end('No File');
-  }
 });
 
 module.exports = app;
