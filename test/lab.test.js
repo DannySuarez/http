3821e83065a89bca0d71826ce308608031fe27f6
DannySuarez <suarezd10@gmail.com>
2019-06-27 10:26:41 -0700

    added more tests, filepath helper

--- test/lab.test.js
+++ test/lab.test.js
@@ -2,6 +2,23 @@ const request = require('supertest');
 const app = require ('../lab/app');
 const fs = require('fs');
 const { join } = require('path');
+const { getFilePath } = require ('../lab/util/filePath');
+
+describe('app routes static server', () => {
+  it('gets file path from url pathname', () => {
+    const filePath = getFilePath('/index.html');
+    expect(filePath).toEqual(expect.stringContaining('public/index.html'));
+  });
+
+  it('gets an index.html if we request it', () => {
+    return request(app)
+      .get('/index.html')
+      .then(res => {
+        expect(res.status).toEqual(200);
+        expect(res.text).toEqual(expect.stringContaining('<h1>Success</h1>'));
+      });
+  });
+});
 
 describe('read a file', () => {
   it('will read index file', done => {
